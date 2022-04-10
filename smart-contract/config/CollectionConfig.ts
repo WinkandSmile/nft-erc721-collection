634e01e3068ac29b4d63c19589ec4d0b09f7a87e
Winkandsmile <wink_smile@outlook.comm>
2022-04-11 00:00:23 +0200

    smartcontact

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,24 +9,24 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: 'TraderBeastsAA',
+  tokenName: 'Trader Beasts A',
+  tokenSymbol: 'TRBAA',
+  hiddenMetadataUri: 'ipfs://QmbmSGdWgE8fDZ4pD4eViGfLDWKWmPXNPgzgUd7WcJ264i/hidden.json',
+  maxSupply: 50,
   whitelistSale: {
-    price: 0.05,
-    maxMintAmountPerTx: 1,
+    price: 0.01,
+    maxMintAmountPerTx: 10,
   },
   preSale: {
-    price: 0.07,
-    maxMintAmountPerTx: 2,
+    price: 0.02,
+    maxMintAmountPerTx: 10,
   },
   publicSale: {
-    price: 0.09,
-    maxMintAmountPerTx: 5,
+    price: 0.02,
+    maxMintAmountPerTx: 10,
   },
-  contractAddress: null,
+  contractAddress: "0x807BB03E753321c10A11390Dc5581BA86C4c7312",
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
